d9ae3c4798530c38ae4695233b171def5702f070
Phil Booth <pmbooth@gmail.com>
2015-01-04 19:06:01 +0000

    Revert "Don't serialise log options."
    This reverts commit d172b942016b17a6cc9573b74e4a6196a7171a72.

--- src/index.js
+++ src/index.js
@@ -64,8 +64,6 @@ function dump (options, results) {
     log = options.log;
     target = path.resolve(options.dump);
 
-    delete results.options.log;
-
     serialiseTimes(results);
 
     log.info('dumping intermediates to `' + target + '`');
