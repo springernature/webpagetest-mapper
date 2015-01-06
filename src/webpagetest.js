12fdb6656774a6fc28d3ffb018558bcca047c873
Phil Booth <pmbooth@gmail.com>
2015-01-06 17:10:21 +0000

    Lengthen poll frequency to 60 seconds.

--- src/webpagetest.js
+++ src/webpagetest.js
@@ -166,7 +166,7 @@ function getResults (options, resultIds) {
     function after (message, resultId, index, metric, error, result) {
         if (result.statusCode < 200) {
             log.info('still waiting for ' + message);
-            return setTimeout(getResult.bind(null, resultId, index, metric), 30000);
+            return setTimeout(getResult.bind(null, resultId, index, metric), 60000);
         }
 
         if (error) {
