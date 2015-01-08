b91f64ca9323799e161bb1ef3e91be271352783a
Phil Booth <pmbooth@gmail.com>
2015-01-08 16:38:08 +0000

    Unit tests for webpagetest.runTests.

--- test/webpagetest.js
+++ test/webpagetest.js
@@ -89,6 +89,291 @@ suite('webpagetest:', function () {
             });
         });
 
+        test('runTests returns a promise', function () {
+            assert.instanceOf(webpagetest.runTests({
+                log: { info: nop, warn: nop, error: nop },
+                tests: []
+            }), Promise);
+        });
+
+        test('webpagetest-api constructor was not called', function () {
+            assert.strictEqual(log.counts.webpagetest, 0);
+        });
+
+        suite('runTests with options:', function () {
+            var resolved, rejected, result, error, done;
+
+            setup(function () {
+                resolved = rejected = false;
+                webpagetest.runTests({
+                    uri: 'test base URI',
+                    key: 'test key',
+                    location: 'test location',
+                    count: 'test count',
+                    connection: 'test connection',
+                    email: 'test email',
+                    log: { info: nop, warn: nop, error: nop },
+                    tests: [
+                        { name: 'Foo', url: 'bar', type: 'home' },
+                        { name: 'BAZ', url: 'qux', type: 'away' }
+                    ]
+                }).then(function (r) {
+                    resolved = true;
+                    result = r;
+                    if (done) {
+                        done();
+                    }
+                }).catch(function (e) {
+                    rejected = true;
+                    error = e;
+                    if (done) {
+                        done();
+                    }
+                });
+            });
+
+            teardown(function () {
+                resolved = rejected = result = error = done = undefined;
+            });
+
+            test('webpagetest-api constructor was called once', function () {
+                assert.strictEqual(log.counts.webpagetest, 1);
+            });
+
+            test('webpagetest-api constructor was called correctly', function () {
+                assert.lengthOf(log.args.webpagetest[0], 1);
+                assert.strictEqual(log.args.webpagetest[0][0], 'test base URI');
+            });
+
+            test('webpagetest-api.runTest was called twice', function () {
+                assert.strictEqual(log.counts.runTest, 2);
+            });
+
+            test('webpagetest-api.runTest was called correctly first time', function () {
+                assert.lengthOf(log.args.runTest[0], 3);
+                assert.strictEqual(log.args.runTest[0][0], 'bar');
+                assert.isObject(log.args.runTest[0][1]);
+                assert.lengthOf(Object.keys(log.args.runTest[0][1]), 37);
+                assert.strictEqual(log.args.runTest[0][1].name, 'Foo');
+                assert.strictEqual(log.args.runTest[0][1].type, 'home');
+                assert.strictEqual(log.args.runTest[0][1].url, log.args.runTest[0][0]);
+                assert.strictEqual(log.args.runTest[0][1].key, 'test key');
+                assert.strictEqual(log.args.runTest[0][1].location, 'test location');
+                assert.strictEqual(log.args.runTest[0][1].runs, 'test count');
+                assert.strictEqual(log.args.runTest[0][1].connectivity, 'test connection');
+                assert.strictEqual(log.args.runTest[0][1].notifyEmail, 'test email');
+                assert.match(log.args.runTest[0][1].label, /^[0-9]{8}-[0-9]{6}-01-foo$/);
+                assert.isTrue(log.args.runTest[0][1].video);
+                assert.isTrue(log.args.runTest[0][1].saveResponseBodies);
+                assert.isTrue(log.args.runTest[0][1].aftRenderingTime);
+                assert.isTrue(log.args.runTest[0][1].medianVideo);
+                assert.isFalse(log.args.runTest[0][1].firstViewOnly);
+                assert.isFalse(log.args.runTest[0][1].private);
+                assert.isFalse(log.args.runTest[0][1].stopAtDocumentComplete);
+                assert.isFalse(log.args.runTest[0][1].tcpDump);
+                assert.isFalse(log.args.runTest[0][1].fullResolutionScreenshot);
+                assert.isFalse(log.args.runTest[0][1].ignoreSSL);
+                assert.isFalse(log.args.runTest[0][1].keepOriginalUserAgent);
+                assert.isFalse(log.args.runTest[0][1].blockAds);
+                assert.isFalse(log.args.runTest[0][1].htmlBody);
+                assert.isFalse(log.args.runTest[0][1].continuous);
+                assert.isFalse(log.args.runTest[0][1].clearCerts);
+                assert.isFalse(log.args.runTest[0][1].emulateMobile);
+                assert.isFalse(log.args.runTest[0][1].timeline);
+                assert.isFalse(log.args.runTest[0][1].netLog);
+                assert.isFalse(log.args.runTest[0][1].forceSoftwareRendering);
+                assert.isFalse(log.args.runTest[0][1].forceSpdy3);
+                assert.isFalse(log.args.runTest[0][1].spdyNoSSL);
+                assert.isFalse(log.args.runTest[0][1].disableOptimization);
+                assert.isFalse(log.args.runTest[0][1].disableScreenshot);
+                assert.isFalse(log.args.runTest[0][1].disableHTTPHeaders);
+                assert.isFalse(log.args.runTest[0][1].disableJavaScript);
+                assert.isFalse(log.args.runTest[0][1].disableThreadedParser);
+                assert.strictEqual(log.args.runTest[0][1].jpegQuality, 85);
+                assert.strictEqual(log.args.runTest[0][1].pollResults, 30);
+                assert.isFunction(log.args.runTest[0][2]);
+            });
+
+            test('webpagetest-api.runTest was called correctly second time', function () {
+                assert.lengthOf(log.args.runTest[1], 3);
+                assert.strictEqual(log.args.runTest[1][0], 'qux');
+                assert.strictEqual(log.args.runTest[1][1].name, 'BAZ');
+                assert.strictEqual(log.args.runTest[1][1].type, 'away');
+                assert.strictEqual(log.args.runTest[1][1].url, log.args.runTest[1][0]);
+                assert.strictEqual(log.args.runTest[1][1].key, 'test key');
+                assert.strictEqual(log.args.runTest[1][1].location, 'test location');
+                assert.strictEqual(log.args.runTest[1][1].runs, 'test count');
+                assert.strictEqual(log.args.runTest[1][1].connectivity, 'test connection');
+                assert.strictEqual(log.args.runTest[1][1].notifyEmail, 'test email');
+                assert.match(log.args.runTest[1][1].label, /^[0-9]{8}-[0-9]{6}-02-baz$/);
+                assert.isFunction(log.args.runTest[1][2]);
+                assert.notStrictEqual(log.args.runTest[0][2], log.args.runTest[1][2]);
+                assert.strictEqual(log.these.runTest[0], log.these.runTest[1]);
+            });
+
+            test('promise is unfulfilled', function () {
+                assert.isFalse(resolved);
+                assert.isFalse(rejected);
+            });
+
+            suite('first callback successful:', function () {
+                setup(function () {
+                    log.args.runTest[0][2](undefined, { data: { id: 'first result' } });
+                });
+
+                test('promise is unfulfilled', function () {
+                    assert.isFalse(resolved);
+                    assert.isFalse(rejected);
+                });
+
+                suite('second callback successful:', function () {
+                    setup(function (d) {
+                        log.args.runTest[1][2](undefined, { data: { id: 'second result' } });
+                        done = d;
+                    });
+
+                    test('promise is resolved', function () {
+                        assert.isTrue(resolved);
+                        assert.isFalse(rejected);
+                    });
+
+                    test('result is correct', function () {
+                        assert.isArray(result);
+                        assert.lengthOf(result, 2);
+
+                        assert.isObject(result[0]);
+                        assert.lengthOf(Object.keys(result[0]), 5);
+                        assert.strictEqual(result[0].name, log.args.runTest[0][1].name);
+                        assert.strictEqual(result[0].type, log.args.runTest[0][1].type);
+                        assert.strictEqual(result[0].url, log.args.runTest[0][1].url);
+                        assert.strictEqual(result[0].label, log.args.runTest[0][1].label);
+                        assert.strictEqual(result[0].id, 'first result');
+                        assert.isUndefined(result[0].error);
+
+                        assert.isObject(result[1]);
+                        assert.strictEqual(result[1].name, log.args.runTest[1][1].name);
+                        assert.strictEqual(result[1].type, log.args.runTest[1][1].type);
+                        assert.strictEqual(result[1].url, log.args.runTest[1][1].url);
+                        assert.strictEqual(result[1].label, log.args.runTest[1][1].label);
+                        assert.strictEqual(result[1].id, 'second result');
+                        assert.isUndefined(result[0].error);
+                    });
+                });
+
+                suite('second callback fails:', function () {
+                    setup(function (d) {
+                        log.args.runTest[1][2]('an error message');
+                        done = d;
+                    });
+
+                    test('promise is resolved', function () {
+                        assert.isTrue(resolved);
+                        assert.isFalse(rejected);
+                    });
+
+                    test('result is correct', function () {
+                        assert.isArray(result);
+                        assert.lengthOf(result, 2);
+
+                        assert.isObject(result[0]);
+                        assert.lengthOf(Object.keys(result[0]), 5);
+
+                        assert.isObject(result[1]);
+                        assert.lengthOf(Object.keys(result[1]), 5);
+                        assert.strictEqual(result[1].name, log.args.runTest[1][1].name);
+                        assert.strictEqual(result[1].type, log.args.runTest[1][1].type);
+                        assert.strictEqual(result[1].url, log.args.runTest[1][1].url);
+                        assert.strictEqual(result[1].label, log.args.runTest[1][1].label);
+                        assert.isUndefined(result[1].id);
+                        assert.strictEqual(result[1].error, 'an error message');
+                    });
+                });
+            });
+
+            suite('second callback successful:', function () {
+                setup(function () {
+                    log.args.runTest[1][2](undefined, { data: { id: 'foo' } });
+                });
+
+                test('promise is unfulfilled', function () {
+                    assert.isFalse(resolved);
+                    assert.isFalse(rejected);
+                });
+
+                suite('first callback successful:', function () {
+                    setup(function (d) {
+                        log.args.runTest[0][2](undefined, { data: { id: 'bar' } });
+                        done = d;
+                    });
+
+                    test('promise is resolved', function () {
+                        assert.isTrue(resolved);
+                        assert.isFalse(rejected);
+                    });
+
+                    test('result is correct', function () {
+                        assert.isArray(result);
+                        assert.lengthOf(result, 2);
+
+                        assert.isObject(result[0]);
+                        assert.strictEqual(result[0].id, 'bar');
+                        assert.isUndefined(result[0].error);
+
+                        assert.isObject(result[1]);
+                        assert.strictEqual(result[1].id, 'foo');
+                        assert.isUndefined(result[0].error);
+                    });
+                });
+
+                suite('first callback fails:', function () {
+                    setup(function (d) {
+                        log.args.runTest[0][2]('a different error message');
+                        done = d;
+                    });
+
+                    test('promise is resolved', function () {
+                        assert.isTrue(resolved);
+                        assert.isFalse(rejected);
+                    });
+
+                    test('result is correct', function () {
+                        assert.isUndefined(result[0].id);
+                        assert.strictEqual(result[0].error, 'a different error message');
+                    });
+                });
+            });
+
+            suite('first callback fails:', function () {
+                setup(function () {
+                    log.args.runTest[0][2]('wibble');
+                });
+
+                test('promise is unfulfilled', function () {
+                    assert.isFalse(resolved);
+                    assert.isFalse(rejected);
+                });
+
+                suite('second callback fails:', function () {
+                    setup(function (d) {
+                        log.args.runTest[1][2]('wobble');
+                        done = d;
+                    });
+
+                    test('promise is resolved', function () {
+                        assert.isTrue(resolved);
+                        assert.isFalse(rejected);
+                    });
+
+                    test('result is correct', function () {
+                        assert.isUndefined(result[0].id);
+                        assert.strictEqual(result[0].error, 'wibble');
+                        assert.isUndefined(result[0].id);
+                        assert.strictEqual(result[1].error, 'wobble');
+                    });
+                });
+            });
+        });
+
         test('getResults function is exported', function () {
             assert.isFunction(webpagetest.getResults);
         });
@@ -105,6 +390,9 @@ suite('webpagetest:', function () {
                     log: { info: nop, warn: nop, error: nop }
                 }, []);
             });
+        });
+
+        suite('getResults:', function () {
         });
     });
 
