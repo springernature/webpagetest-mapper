53786c93de2c9dc6f40765002c338c7943a5afc5
Phil Booth <pmbooth@gmail.com>
2015-04-21 21:20:48 +0100

    Fix stupid require path error.

--- src/cli.js
+++ src/cli.js
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU General Public License along with
 // webpagetest-mapper. If not, see <http://www.gnu.org/licenses/>.
 
-/*globals require, process, console */
+/*globals require, __dirname, process, console */
 
 'use strict';
 
@@ -26,7 +26,7 @@ var fs, path, cli, impl, options;
 fs = require('fs');
 path = require('path');
 cli = require('commander');
-impl = require('.');
+impl = require(__dirname);
 options = require('./options');
 
 options.cli.forEach(function (option) {
